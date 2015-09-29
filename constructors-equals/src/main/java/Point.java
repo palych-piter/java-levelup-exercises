93a98eaa1c888f0f73818d4d8bc51a0138838194
palych-piter <andr.bespalov@gmail.com>
2015-09-30 00:29:09 +0300

    Submit code to investigate constructors and equals

--- constructors-equals/src/main/java/Point.java
+++ constructors-equals/src/main/java/Point.java
@@ -28,8 +28,10 @@ public class Point {
     public static void main(String[] args) {
         Point point1 = new Point(10,20);
         Point point2 = new Point(10,20);
-        System.out.println("Compare points: " + (point1 == point2));
-        System.out.println("Compare points by equals: " + point1.equals(point2));
+        System.out.println("Compare points: " +
+                (point1 == point2));
+        System.out.println("Compare points by equals: " +
+                point1.equals(point2));
     }
 
 }
